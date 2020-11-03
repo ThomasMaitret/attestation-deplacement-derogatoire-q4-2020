42d8b943a88c9795688ffe64cd864356e0e0a053
Thomas Maitret Richard <thomas.maitret@laposte.net>
2020-11-03 14:58:39 +0100

    remove service worker

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,6 @@ import 'bootstrap/dist/css/bootstrap-reboot.min.css';
 
 import '../css/main.css';
 
-import './check-updates';
 import { prepareForm } from './form-util';
 import { createForm } from './form';
 
