b5111c9b389911880183ea7390c7d0f09b86bb4f
Thomas Maitret Richard <thomas.maitret@laposte.net>
2020-11-03 14:49:32 +0100

    remove standard

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,22 +4,8 @@ module.exports = {
     node: true,
     browser: true,
   },
-  extends: ['standard'],
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'comma-dangle': [2, 'always-multiline'],
-    'no-var': 2,
-  },
-  overrides: [
-    {
-      files: ['**/__tests__/*.js', '**/tests/unit/**/*.spec.js'],
-      env: {
-        jest: true,
-      },
-    },
-  ],
+  extends: ['eslint:recommended'],
   parserOptions: {
     parser: 'babel-eslint',
   },
-}
+};
